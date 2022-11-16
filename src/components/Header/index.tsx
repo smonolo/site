9b1322da2842f052de0d7fea3e5a5cb4e263547a
s.monolo <lu3mtex@gmail.com>
2022-11-16 17:47:26 +0100

    feat: load social links from supabase

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,35 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 import { StyledGrid } from "./styles";
+
+import { Social } from "./types";
+import { supabase } from "../../lib/supabase";
 
 import data from '../../data/header.json';
 
 export default function Header() {
+    const [socials, setSocials] = useState<Social[]>([]);
+    const [error, setError] = useState<boolean>(false);
+
     const router = useRouter();
 
-    const { items, socials } = data;
+    const { items } = data;
+
+    useEffect(() => {
+        fetchSocials();
+    }, []);
+
+    const fetchSocials = async () => {
+        const { data, error: err } = await supabase.from('socials').select();
+
+        if (err) {
+            return setError(true);
+        }
+
+        setSocials(data);
+    };
 
     return (
         <StyledGrid>
@@ -34,13 +55,14 @@ export default function Header() {
                 ))}
             </div>
             <div className='flex'>
+                {error && <div className='error no-top'>Could not load socials</div>}
                 {socials.map(({ icon, url }) => (
                     <a
                         key={'social-' + icon.replace('bi bi-', '')}
                         href={url}
                         target='_blank'
                     >
-                        <i className={icon + ' item'} />
+                        <i className={'bi bi-' + icon + ' item'} />
                     </a>
                 ))}
             </div>
---
+++ src/components/Header/types.ts
@@ -0,0 +1,5 @@
+export interface Social {
+    id: number;
+    icon: string;
+    url: string;
+}
