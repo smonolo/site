9e6bfe3b1fc021073434defb6f1de0c7dd138c19
s.monolo <lu3mtex@gmail.com>
2023-01-01 14:39:17 +0100

    add github link to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
 import { StyledRow } from "./styles";
 
+import info from '../../../package.json';
+
 export default function Footer() {
     return (
         <StyledRow>
             <div>© Stefano Monolo. Partita IVA IT11729910965</div>
+            <a href={info.repository} target='_blank'>
+                <i className='bi bi-github' /> View on GitHub
+            </a>
         </StyledRow>
     );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,7 +4,7 @@ export const StyledRow = styled.div`
     width: 100%;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     padding: 25px;
     box-sizing: border-box;
     border-top: 2px solid #0a0a0f;
@@ -12,5 +12,29 @@ export const StyledRow = styled.div`
 
     & > div {
         color: #aaa;
+        font-size: 14px;
+    }
+
+    & > a {
+        display: block;
+        text-decoration: none;
+        padding: 6px 14px;
+        border-radius: 10px;
+        background-color: #0a0a0f;
+        box-sizing: border-box;
+        font-size: 14px;
+        color: #aaa;
+        transition: background-color .3s;
+        display: flex;
+        align-items: center;
+        gap: 7px;
+
+        &:hover {
+            background-color: #0f0f16;
+        }
+
+        & > i {
+            font-size: 14px;
+        }
     }
 `;
